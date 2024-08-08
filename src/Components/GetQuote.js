38e950e415e9911ba3c2474892b2f38b57d6235b
Najeeba <najeebaqarqin@gmail.com>
2024-08-08 19:24:46 +0430

    Added function to fetch the quotes from

--- src/Components/GetQuote.js
+++ src/Components/GetQuote.js
@@ -1,14 +1,27 @@
 import React from "react";
 
 class GetQuote extends React.Component {
-      constructor(props) {
-      super(props);
-      this.state = {
-        text:"",
-        author: "",
-        url:""
-      };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: "",
+      author: "",
+      url: ""
+    };
+  }
+  quote() {
+    fetch("https://type.fit/api/quotes")
+      .then(response => response.json())
+      .then((data) => {
+        var item = data[Math.floor(Math.random() * data.length)]
+        this.setState({
+          text: item.text,
+          author: item.author,
+          url: "https://twitter.com/intent/tweet?text=" + item.text + " - " + item.author
+        });
+      })
+      .catch(error => console.log(error));
+  }
 }
 
 export default GetQuote;
