03bfd045f2ec494a05ea11f1430c20e94c220376
Najeeba <najeebaqarqin@gmail.com>
2024-08-08 19:23:02 +0430

    Added constructor for props and this methods

--- src/Components/GetQuote.js
+++ src/Components/GetQuote.js
@@ -1,1 +1,14 @@
 import React from "react";
+
+class GetQuote extends React.Component {
+      constructor(props) {
+      super(props);
+      this.state = {
+        text:"",
+        author: "",
+        url:""
+      };
+    }
+}
+
+export default GetQuote;
